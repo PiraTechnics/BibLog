6e7931bb6063690a0799554a79ba6a0f3d1b828c
PiraTechnics <devin1younge@gmail.com>
2023-04-17 14:39:36 -0700

    Delete Entry and toggle Read State Functionality

--- biblog.js
+++ biblog.js
@@ -9,17 +9,19 @@ displayAllBooks();
 //Set a listener on form submission to add book, then toggle form visibility off again
 document.querySelector("#addBookForm").addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log("Sucessfully prevented default submit behavior!");
 
     //Get info in form and add (form fields are required in HTML)
-    const formData = document.querySelectorAll("input");
-    const newEntry = new Book(formData[0].value, formData[1].value, formData[2].value, formData[3].checked);
-    
+    const entryTitle = document.querySelector("input#bookTitle");
+    const entryAuthor = document.querySelector("input#bookAuthor");
+    const entryPages = document.querySelector("input#bookPages");
+    const entryRead = document.querySelector("input#bookRead");
+
+    const newEntry = new Book(entryTitle.value, entryAuthor.value, entryPages.value, entryRead.checked);
+
     myLibrary.push(newEntry);
     createBookEntry(newEntry);
-    
-    console.log(newEntry);
 
+    console.log(newEntry);
 
     //Turn off visibility on div, then return (instead of sending POST)
     document.getElementById("addBookFormContainer").classList.remove("show");
@@ -33,42 +35,95 @@ function Book(title, author, pages, readState) {
     this.pages = pages;
     this.readState = readState;
     this.readYet = readState ? "yes" : "no";
+}
 
-    this.info = function() {
-        return title + ", by " + author + ", " + pages + " pages, " + (readState ? "has been read" : "unread");
-    }
+//Define toggle read state on prototype of Book
+//Note: We could do this on the book object itself, but that would create the function on each instance of 'Book', which would eat a lot of memory at scale.
+Book.prototype.setReadState = function(newState) {
+    this.readState = newState;
+    //Update readYet to match
+    this.readYet = this.readState ? "yes" : "no";
+}
+
+Book.prototype.getInfo = function() {
+    return this.title + ", by " + this.author + ", " + this.pages + " pages, " + (this.readState ? "read" : "unread");
 }
 
 function displayAllBooks() {
     myLibrary.forEach(book => {
-        createBookEntry(book);    
+        createBookEntry(book);
     });
 }
 
-//Function to only display a single book -- helper function for updating
+//Remove Book Entry Function (and remove from array)
+function removeBook(index) {
+    toRemove = document.querySelector('[data-row-num="' + index + '"]');
+    delete myLibrary[index]; //doesn't reindex, but that's fine for now
+    toRemove.remove();
+    console.log(myLibrary);
+}
+
+//Add Book Table Entry Function
 function createBookEntry(book) {
     const newRow = document.createElement("tr");
-        //need to append each cell to the row, and then appendChild the row to the tbody element
-        
-        //create each table cell for the entry
-        const titleCell = document.createElement("td");
-        const authorCell =  document.createElement("td");
-        const pagesCell = document.createElement("td");
-        const readCell = document.createElement("td");
-        
-        //Get all data for the entry and append to proper cell
-        const titleEntry = document.createTextNode(book.title);
-        titleCell.appendChild(titleEntry);
-        const authorEntry = document.createTextNode(book.author);
-        authorCell.appendChild(authorEntry);
-        const pagesEntry = document.createTextNode(book.pages);
-        pagesCell.appendChild(pagesEntry);
-        const readEntry = document.createTextNode(book.readYet);
-        readCell.appendChild(readEntry);
+    //Set row number as data-attribute
+    newRow.dataset.rowNum = myLibrary.indexOf(book);
+    //newRow.classList.add("align-middle");
 
-        //Append all cells to row, then row to tbody in DOM
-        newRow.append(titleCell, authorCell, pagesCell, readCell);
-        document.getElementById("bookEntries").appendChild(newRow);
+    //create each table cell for the entry)
+    const titleCell = document.createElement("td");
+    const authorCell = document.createElement("td");
+    const pagesCell = document.createElement("td");
+    const readCell = document.createElement("td");
 
-        console.log("Added entry: " + book.info());
+    //Get all data for the entry and append to proper cell
+    const titleEntry = document.createTextNode(book.title);
+    titleCell.appendChild(titleEntry);
+    const authorEntry = document.createTextNode(book.author);
+    authorCell.appendChild(authorEntry);
+    const pagesEntry = document.createTextNode(book.pages);
+    pagesCell.appendChild(pagesEntry);
+
+    //Add toggle switch for "has read"
+    const readSwitch = document.createElement("input");
+    const readlabel = document.createElement("label");
+
+    readSwitch.classList.add("form-check-input");
+    readSwitch.type = "checkbox";
+    readSwitch.role = "switch";
+    readSwitch.id = "readSwitch-" + newRow.dataset.rowNum;
+    readSwitch.checked = book.readState;
+    readSwitch.oninput = function () {
+        book.setReadState(readSwitch.checked);
+        //Update entry on table
+        readlabel.innerText = book.readYet;
+        console.log(book.getInfo());
+    }
+
+    readlabel.classList.add("form-check-label");
+    readlabel.for = readSwitch.id;
+    readlabel.innerText = book.readYet;
+
+    const readForm = document.createElement("div");
+    readForm.classList.add("form-check", "form-switch");
+    readForm.append(readSwitch, readlabel);
+
+    readCell.appendChild(readForm);
+
+    //Add button to delete row (from library and table)
+    const removeBookCell = document.createElement("td");
+    const removeBookButton = document.createElement("button");
+    removeBookButton.classList.add("btn", "btn-outline-danger", "btn-sm");
+    removeBookButton.innerText = "Remove";
+    removeBookButton.onclick = function () {
+        removeBook(newRow.dataset.rowNum);
+    }
+    removeBookCell.appendChild(removeBookButton);
+
+    //Append all cells to row, then row to tbody in DOM
+    newRow.append(titleCell, authorCell, pagesCell, readCell, removeBookCell);
+    document.getElementById("bookEntries").appendChild(newRow);
+
+    //For some reason, this throws a an error: getInfo not a function -- some kind of scope issue -___-
+    //console.log("Added entry: " + book.getInfo());
 }
